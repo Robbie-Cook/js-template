11aee077d50a16d80549d3e2cdb28ad852fccbd0
Robbie Cook <robbieck7@gmail.com>
2020-05-12 20:45:50 +1200

    Started to create script

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const NodeHelper = require("@robbie-cook/node-helper");
+const path_1 = require("path");
 const yargs = require('yargs').argv;
-console.log('Pulling in starter...');
 const repoAddress = 'https://github.com/Robbie-Cook/typescript-starter';
 /**
  * Main function for this library
  */
 async function run() {
-    // console.log(await NodeHelper.execute(`git clone ${repoAddress} ${yarg}`));
-    console.log(yargs._);
+    var _a;
+    console.log('Pulling in starter...');
+    const outputDir = (_a = yargs.outputDir) !== null && _a !== void 0 ? _a : ".";
+    const tempOutputDir = path_1.default.resolve(outputDir, 'temp');
+    console.log(await NodeHelper.execute(`git clone ${repoAddress} ${tempOutputDir !== null && tempOutputDir !== void 0 ? tempOutputDir : '.'}`));
+    console.log('Removing git info...');
+    console.log(await NodeHelper.execute(`rm -rf ${path_1.default.resolve(tempOutputDir, ".git")}`));
+    console.log(`Moving files from temp to ${outputDir}`);
+    console.log(await NodeHelper.execute(`mv -r ${tempOutputDir}/ ${outputDir}`));
 }
 run();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as NodeHelper from "@robbie-cook/node-helper";
+import path from "path";
 const yargs = require('yargs').argv;
 
-console.log('Pulling in starter...');
 
 const repoAddress = 'https://github.com/Robbie-Cook/typescript-starter';
 
@@ -9,7 +9,18 @@ const repoAddress = 'https://github.com/Robbie-Cook/typescript-starter';
  * Main function for this library
  */
 async function run() {
-  console.log(await NodeHelper.execute(`git clone ${repoAddress} ${yargs.outputDir ?? '.'}`));
+  console.log('Pulling in starter...');
+  const outputDir = yargs.outputDir ?? ".";
+
+  const tempOutputDir = path.resolve(outputDir, 'temp');
+
+  console.log(await NodeHelper.execute(`git clone ${repoAddress} ${tempOutputDir ?? '.'}`));
+
+  console.log('Removing git info...')
+  console.log(await NodeHelper.execute(`rm -rf ${path.resolve(tempOutputDir, ".git")}`));
+
+  console.log(`Moving files from temp to ${outputDir}`);
+  console.log(await NodeHelper.execute(`mv -r ${tempOutputDir}/ ${outputDir}`));
 }
 
 run();
